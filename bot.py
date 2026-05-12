d6fe1a01465b2e066d74d90eaea2274777348c7f
adsensefortrsv-bit <adsensefortrsv@gmail.com>
2026-05-12 20:49:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,40 +10,40 @@ from telegram.ext import (
     ContextTypes
 )
 
-# =========================
+# ====================================
 # BOT TOKEN
-# =========================
+# ====================================
 BOT_TOKEN = "8488142902:AAGL781OPUQoWAe5WwUiFIR8lS-8vG0JFDg"
 
-# =========================
+# ====================================
 # PHOTO / BANNER
-# =========================
+# ====================================
 WELCOME_IMAGE = "https://i.ibb.co/HDf1gWgm/6060052766098395538.jpg"
 
-# =========================
+# ====================================
 # WELCOME MESSAGE
-# =========================
+# ====================================
 WELCOME_TEXT = """
 ╔══❖•ೋ° °ೋ•❖══╗
-      💎 𝗧𝗥𝗦𝗩 𝗘𝗗𝗜𝗧𝗭 💎
+       💎 𝗧𝗥𝗦𝗩 𝗘𝗗𝗜𝗧𝗭 💎
 ╚══❖•ೋ° °ೋ•❖══╝
 
 🚀 <b>WELCOME TO TRSV EDITZ BOT</b>
 
 ⚡️ Auto YouTube Upload Notifications
-🎬 New Videos Instantly
-🔥 Stay Connected With Us
+🎬 Get New Videos Instantly
+🔥 Stay Connected With Our Community
 
 ━━━━━━━━━━━━━━━━━━
 
-👑 <b>OFFICIAL PLATFORMS</b>
+👑 <b>JOIN ALL OFFICIAL PLATFORMS</b>
 
-🔥 Enjoy Premium Experience
+💜 Enjoy Premium Style Experience
 """
 
-# =========================
+# ====================================
 # START COMMAND
-# =========================
+# ====================================
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     keyboard = [
@@ -69,15 +69,15 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
         [
             InlineKeyboardButton(
-                "🌐 FACEBOOK",
-                url="https://facebook.com/"
+                "🔥 WHATSAPP CHANNEL",
+                url="https://whatsapp.com/channel/0029VbCcbE9Au3aYo9SUZ61c"
             )
         ],
 
         [
             InlineKeyboardButton(
-                "🔥 WHATSAPP",
-                url="https://whatsapp.com/channel/0029VbCcbE9Au3aYo9SUZ61c"
+                "🌐 FACEBOOK PAGE",
+                url="https://facebook.com/share/1CqE63Lf7S/"
             )
         ]
 
@@ -92,13 +92,13 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         reply_markup=reply_markup
     )
 
-# =========================
+# ====================================
 # BOT START
-# =========================
+# ====================================
 app = Application.builder().token(BOT_TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
 
-print("🔥 BOT RUNNING SUCCESSFULLY 🔥")
+print("🔥 TRSV EDITZ BOT RUNNING 🔥")
 
 app.run_polling()
